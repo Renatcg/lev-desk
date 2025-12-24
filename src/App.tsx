3aceac20577707858fe88e8bf79952850b85dced
Renat CG <renatcg@github.com>
2025-12-23 20:16:23 -0500

    fix: revert to /lev-desk/ subdirectory for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/lev-desk">
           <AuthProvider>
           <Routes>
             <Route path="/" element={<Navigate to="/login" replace />} />
